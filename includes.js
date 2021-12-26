aabb2b4c7e843cafdc473f06f26856f4a81872d0
webpriya <priyachouhan1019@gmail.com>
2021-12-26 08:07:28 -0800

    includes.ja program added

---
+++ includes.js
@@ -0,0 +1,12 @@
+let party = ["piyu", "sachin", "shanu", "priya"];
+const check = party.includes("priya");
+if(check === party){
+    console.log("you are invited");
+}
+else{
+    console.log("you are not invited"); 
+}
+
+
+
+
